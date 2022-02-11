ee24bfea75433851cc1d861ebe3bc12902bc1c53
Alihan-Zakishew <zakishewalihan@gmail.com>
2022-02-11 20:38:57 +0300

    Добавьте файлы проекта.

---
+++ Geometrus.Алихан/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Geometrus.Алихан
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            Console.Write("Длина: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            
+
+            int area = A * A;
+
+            Console.WriteLine("Площадь квадрата: {0}", area);
+            Console.ReadLine();
+        }
+    }
+}
